6115ba8d0eba56d10502bfa476b2d5f56438ac70
unknown <assakay92@gmail.com>
2024-03-07 16:01:25 +0200

    carousel

---
+++ build/js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){"use strict";$(".one-time").slick({dots:!0,infinite:!0,speed:1e3,slidesToShow:3,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:1e3})}();
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import burger from './burger.js'
 
-
+$(".one-time").slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1000
+  });
+  
