f9d9f3ddb54811790c7538dcbcb6a9bad91f3067
sereda_pavel <assakay92@gmail.com>
2024-03-08 10:17:43 +0200

    new commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,24 @@
 import burger from './burger.js'
 
 $(".one-time").slick({
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 3,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 1000
-  });
-  
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  centerMode: true
+});
+
+const commercialLighting = document.querySelector('.commercial_lighting');
+
+window.addEventListener('scroll', function() {
+    const blockTop = commercialLighting.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (blockTop <= windowHeight * .7) {
+      commercialLighting.classList.add('active_scroll')
+  }else{
+    commercialLighting.classList.remove('active_scroll')
+  }
+});
